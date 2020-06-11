d006311392e74dfdafe8ecf7efce26c19d851244
Joshua McWilliams <jtmacnb@gmail.com>
2020-06-11 14:54:58 -0500

    clean up

--- app/src/main/java/com/mcwilliams/theninjamethod/TheNinjaMethodApplication.kt
+++ app/src/main/java/com/mcwilliams/theninjamethod/TheNinjaMethodApplication.kt
@@ -1,21 +1,10 @@
 package com.mcwilliams.theninjamethod
 
 import android.app.Application
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TheNinjaMethodApplication : Application() {
-
-//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-//        return DaggerAppComponent.factory().create(this)
-//    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-    }
 
     companion object {
         var isUserLoggedIn = false
